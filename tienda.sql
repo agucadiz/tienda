620eb0150010745181c478d46e8cfaab77502fd3
agucadiz <agustin.pedrote@iesdonana.org>
2022-11-08 11:22:32 +0100

    tienda.sql creada

---
+++ tienda.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS articulos CASCADE;
+
+CREATE TABLE articulos (
+    id          bigserial       PRIMARY KEY,
+    codigo      varchar(13)     NOT NULL UNIQUE,
+    descripcion varchar(255)    NOT NULL,
+    precio      numeric(7, 2)   NOT NULL
+);
+
+--Carga inicial de datos de prueba:
+
+INSERT INTO articulos (codigo, descripcion, precio)
+VALUES ('2138972198', 'Yogurt piña', 200.50),
+       ('4324324324', 'Bollicao', 50.10),
+       ('3243243243', 'PC', 1001.10);
